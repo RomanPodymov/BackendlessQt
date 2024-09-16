1427b13396e540ee2f4625bbef88a8a52da218c9
Roman Podymov <podymfrombryansk@yandex.com>
2024-09-16 17:04:46 +0200

    Dynamic properties

--- BackendlessUser.hpp
+++ BackendlessUser.hpp
@@ -9,18 +9,49 @@
 #ifndef BACKENDLESS_USER_H
 #define BACKENDLESS_USER_H
 
+#include <QMap>
 #include <QString>
 
-struct BackendlessRegisterUser {
-    QString email;
-    QString name;
-    QString password;
+class BackendlessRegisterUserRepresentable {
+public:
+    virtual QMap<QString, QString> getAllParams() = 0;
+};
 
+struct BasicBackendlessRegisterUser: BackendlessRegisterUserRepresentable {
+public:
+    BasicBackendlessRegisterUser(
+        QString _email,
+        QString _password
+    ): email(_email), password(_password) { }
+
+    QMap<QString, QString> getAllParams() {
+        return {
+            {"email", email},
+            {"password", password}
+        };
+    }
+
+protected:
+    QString email;
+    QString password;
+};
+
+struct BackendlessRegisterUser: BasicBackendlessRegisterUser {
+public:
     BackendlessRegisterUser(
         QString _email,
-        QString _name,
-        QString _password
-    ): email(_email), name(_name), password(_password) { }
+        QString _password,
+        QString _name
+    ): BasicBackendlessRegisterUser(_email, _password), name(_name) { }
+
+    QMap<QString, QString> getAllParams() {
+        QMap<QString, QString> result = {{"name", name}};
+        result.insert(BasicBackendlessRegisterUser::getAllParams());
+        return result;
+    }
+
+protected:
+    QString name;
 };
 
 struct BackendlessSignInUser {
--- BackendlessUserAPI.cpp
+++ BackendlessUserAPI.cpp
@@ -27,11 +27,9 @@ void BackendlessUserAPI::registerUser(BackendlessRegisterUser user) {
         networkAccessManager,
         this,
         endpoint + appId + "/" + apiKey + "/users/register",
-        {
-            {"email", user.email},
-            {"name", user.name},
-            {"password", user.password}
-        }, true, [&](QNetworkReply* reply){
+        user.getAllParams(),
+        true,
+        [&](QNetworkReply* reply){
             auto replyValue = reply->readAll();
             qDebug() << replyValue;
 
