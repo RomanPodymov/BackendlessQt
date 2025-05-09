06f243441a172ba0167d614a7a1ab7d15cd55dc6
Roman Podymov <podymfrombryansk@yandex.com>
2025-05-09 12:07:02 +0200

    Less NULL

--- BackendlessAPI.cpp
+++ BackendlessAPI.cpp
@@ -52,7 +52,9 @@ void BackendlessAPI::deleteItemFromTable(QString tableName, QString objectId) {
             qDebug() << replyValue;
             extractResult<DeletionResult>(
                 replyValue,
-                nullptr,
+                [](auto obj){
+                    return new DeletionResult(obj);
+                },
                 [&](auto result) {
                     emit deleteItemFromTableSuccess(result);
                 },
